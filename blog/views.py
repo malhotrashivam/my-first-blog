1393930df4c475bf7b3ec56df0e713d07c6dce2c
Shivam Malhotra <shivammalhotra@Shivams-MacBook-Pro.local>
2015-12-05 17:03:22 +0530

    HTML added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+# A view is a place where we put the "logic" of our application. 
+# It will request information from the model you created before and pass it to a template
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    # We created a function (def) called post_list that takes request and return a function 
+    # render that will render (put together) our template blog/post_list.html
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    # for every URL that starts with admin/ Django will find a corresponding view. 
+    # In this case we're including a lot of admin URLs so it isn't all packed into 
+    # this small file -- it's more readable and cleaner
+
+    url(r'', include('blog.urls')),
+    # everything that comes into 'http://127.0.0.1:8000/' will be redirected to blog.urls 
+    # and looked for further instructions there
 ]
