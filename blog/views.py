ab8eca939ad8496662b6cc1b4c3fbf25039a8360
Shivam Malhotra <shivammalhotra@Shivams-MacBook-Pro.local>
2015-12-05 23:32:53 +0530

    Blogs printed

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,19 @@
 # It will request information from the model you created before and pass it to a template
 
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone	# Sorting by published dates, so we need timezone
+from .models import Post 	# Import the post model , . because in the same directory
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # The query is for printing blog posts sorted by published_date
+    # So posts will act as our queryset	
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
     # We created a function (def) called post_list that takes request and return a function 
     # render that will render (put together) our template blog/post_list.html
+    # 
+    # In our post_list view we will need to take models we want to display and pass them to 
+    # the template. In a view we decide what (model) will be displayed in a template.
+    # {} is a place in which we can add some things for the template to use, so we have given the 
+    # list of posts a name
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog',     # This is something we changed
 )
 
 MIDDLEWARE_CLASSES = (
@@ -45,7 +45,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.MIDDLEWARE_CLASSESeware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
